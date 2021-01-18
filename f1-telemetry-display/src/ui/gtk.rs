d1509ee714f39571133591e848b437013c6b742c
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-01-18 11:03:47 -0500

    Load css from custom.css

--- f1-telemetry-display/src/ui/gtk.rs
+++ f1-telemetry-display/src/ui/gtk.rs
@@ -52,6 +52,14 @@ impl Ui for GTKUi {
     fn run(&mut self, stream: Stream) {
         self.app.connect_startup(move |app| {
             let provider = gtk::CssProvider::new();
+            provider.load_from_path("custom.css").unwrap_or_default();
+            gtk::StyleContext::add_provider_for_screen(
+                &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
+                &provider,
+                gtk::STYLE_PROVIDER_PRIORITY_USER,
+            );
+
+            let provider = gtk::CssProvider::new();
             provider
                 .load_from_data(STYLE.as_bytes())
                 .expect("Failed to load CSS");
