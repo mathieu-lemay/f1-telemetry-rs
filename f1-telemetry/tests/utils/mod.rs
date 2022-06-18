a0f2b9e094d4148bba1d7b970225b9e6af757fbd
Mathieu Lemay <acidrain1@gmail.com>
2022-06-18 11:46:18 -0400

    Add utils::send_raw_data for tests

--- f1-telemetry/tests/utils/mod.rs
+++ f1-telemetry/tests/utils/mod.rs
@@ -15,3 +15,17 @@ pub fn get_connected_socket(stream: &Stream) -> &UdpSocket {
 
     s
 }
+
+pub fn send_raw_data(stream: &Stream, data: &str) {
+    let data = hex::decode(data).unwrap();
+    let socket = get_connected_socket(stream);
+
+    let res = socket.send(&data);
+
+    assert!(res.is_ok(), "Error sending data to socket");
+    assert_eq!(
+        res.unwrap(),
+        data.len(),
+        "Invalid amount of data sent through socket"
+    );
+}
