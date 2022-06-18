358d0266c9c1027a5dd90fc05e0a1778c1298923
Mathieu Lemay <acidrain1@gmail.com>
2022-06-18 10:26:12 -0400

    Improve integration test for 2019 Car Setups Packet

--- f1-telemetry/src/packet/car_setup.rs
+++ f1-telemetry/src/packet/car_setup.rs
@@ -28,7 +28,7 @@ use super::header::PacketHeader;
 /// fuel_load               Fuel load
 ///
 /// [`PacketCarSetupData`]: ./struct.CarSetupData.html
-#[derive(Debug, CopyGetters)]
+#[derive(Debug, PartialEq, CopyGetters)]
 #[getset(get_copy = "pub")]
 pub struct CarSetupData {
     front_wing: u8,
@@ -54,6 +54,49 @@ pub struct CarSetupData {
 
 #[allow(clippy::too_many_arguments)]
 impl CarSetupData {
+    pub fn new(
+        front_wing: u8,
+        rear_wing: u8,
+        on_throttle: u8,
+        off_throttle: u8,
+        front_camber: f32,
+        rear_camber: f32,
+        front_toe: f32,
+        rear_toe: f32,
+        front_suspension: u8,
+        rear_suspension: u8,
+        front_anti_roll_bar: u8,
+        rear_anti_roll_bar: u8,
+        front_suspension_height: u8,
+        rear_suspension_height: u8,
+        brake_pressure: u8,
+        brake_bias: u8,
+        tyres_pressure: WheelData<f32>,
+        ballast: u8,
+        fuel_load: f32,
+    ) -> Self {
+        CarSetupData {
+            front_wing,
+            rear_wing,
+            on_throttle,
+            off_throttle,
+            front_camber,
+            rear_camber,
+            front_toe,
+            rear_toe,
+            front_suspension,
+            rear_suspension,
+            front_anti_roll_bar,
+            rear_anti_roll_bar,
+            front_suspension_height,
+            rear_suspension_height,
+            brake_pressure,
+            brake_bias,
+            tyres_pressure,
+            ballast,
+            fuel_load,
+        }
+    }
     pub(crate) fn from_2019(
         front_wing: u8,
         rear_wing: u8,
@@ -165,7 +208,7 @@ impl CarSetupData {
 /// header:     Header
 /// car_setups: List of car setups (20)
 /// ```
-#[derive(Debug, Getters)]
+#[derive(Debug, PartialEq, Getters)]
 #[getset(get = "pub")]
 pub struct PacketCarSetupData {
     header: PacketHeader,
@@ -173,7 +216,7 @@ pub struct PacketCarSetupData {
 }
 
 impl PacketCarSetupData {
-    pub(crate) fn new(header: PacketHeader, car_setups: Vec<CarSetupData>) -> PacketCarSetupData {
+    pub fn new(header: PacketHeader, car_setups: Vec<CarSetupData>) -> PacketCarSetupData {
         PacketCarSetupData { header, car_setups }
     }
 }
